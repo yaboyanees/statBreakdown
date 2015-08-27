09d71a7cb88aa717c93dbf238f8a7049a1c6b31f
yaboyanees <a.merzi@yahoo.com>
2015-08-26 21:22:29 -0400

    ordered by team name | adjusted 3d scatter plot size for current mean

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,6 +11,7 @@ class HomeController < ApplicationController
  			@team1s2dm = @team1.pluck("Season2dateMean")
  			@team1name = @team1.uniq.pluck("team1")
  			@team1result = @team1.pluck("win")
+ 			@team1current = @team1.pluck("CurrentMeanTrend").last
 
  			@team2 = Stat.search(params[:secondTeam])
   			@team2name = @team2.uniq.pluck("team1")
@@ -18,6 +19,7 @@ class HomeController < ApplicationController
  			@team2off = @team2.pluck("OFFGameMean")
  			@team2def = @team2.pluck("DEFGameMean")
  			@team2s2dm = @team2.pluck("Season2dateMean")
+ 			@team2current = @team2.pluck("CurrentMeanTrend").last
  			
  			@teamValue1 = @team1.pluck("Season2dateMean", "Season2dateOFFMean", "Season2dateDEFMean").last
  			@teamValue2 = @team2.pluck("Season2dateMean", "Season2dateOFFMean", "Season2dateDEFMean").last
@@ -30,7 +32,7 @@ class HomeController < ApplicationController
  		else
  			@stats = Stat.all
  		end
- 		@teamNames = Stat.uniq.pluck(:team1)
+ 		@teamNames = Stat.order(:team1).uniq.pluck(:team1)
 	end
 	
 	def show
