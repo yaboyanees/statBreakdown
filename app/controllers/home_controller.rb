c8060cd5eb15294d8995a2c0e4c6b873d975b9f7
yaboyanees <a.merzi@yahoo.com>
2015-08-28 17:20:08 -0400

    fixed team value queries to return correct values

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,20 +4,23 @@ class HomeController < ApplicationController
  			@team1 = Stat.search(params[:firstTeam]) 
  			
  			@team1wk = @team1.order("week").uniq.pluck("week")
- 			@team1gt = @team1.pluck("GameMean")
  			@team1gt = @team1.pluck("win", "GameMean")
- 			@team1off = @team1.pluck("OFFGameMean")
- 			@team1def = @team1.pluck("DEFGameMean")
+ 			@team1off = @team1.pluck("win", "OFFGameMean")
+ 			@team1offVal = @team1.pluck("OFFGameMean")
+ 			@team1def = @team1.pluck("win", "DEFGameMean")
+ 			@team1defVal = @team1.pluck("DEFGameMean")
  			@team1s2dm = @team1.pluck("Season2dateMean")
  			@team1name = @team1.uniq.pluck("team1")
- 			@team1result = @team1.pluck("win")
+#  			@team1result = @team1.pluck("win")
  			@team1current = @team1.pluck("CurrentMeanTrend").last
 
  			@team2 = Stat.search(params[:secondTeam])
   			@team2name = @team2.uniq.pluck("team1")
-  			@team2gt = @team2.pluck("win", "GameMean")
- 			@team2off = @team2.pluck("OFFGameMean")
- 			@team2def = @team2.pluck("DEFGameMean")
+ 			@team2gt = @team2.pluck("win", "GameMean")
+ 			@team2off = @team2.pluck("win", "OFFGameMean")
+ 			@team2offVal = @team2.pluck("OFFGameMean")
+ 			@team2def = @team2.pluck("win", "DEFGameMean")
+ 			@team2defVal = @team2.pluck("DEFGameMean")
  			@team2s2dm = @team2.pluck("Season2dateMean")
  			@team2current = @team2.pluck("CurrentMeanTrend").last
  			
