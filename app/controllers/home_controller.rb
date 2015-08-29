a273c8ea7324fadfbe384ee66beb0bd2ad6ddd30
yaboyanees <a.merzi@yahoo.com>
2015-08-29 17:04:30 -0400

    added table to show each team results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,36 +1,48 @@
 class HomeController < ApplicationController
 	def index
  		if params[:firstTeam]
- 			@team1 = Stat.search(params[:firstTeam]) 
- 			
- 			@team1wk = @team1.order("week").uniq.pluck("week")
- 			@team1gt = @team1.pluck("win", "GameMean")
- 			@team1off = @team1.pluck("win", "OFFGameMean")
- 			@team1offVal = @team1.pluck("OFFGameMean")
- 			@team1def = @team1.pluck("win", "DEFGameMean")
- 			@team1defVal = @team1.pluck("DEFGameMean")
- 			@team1s2dm = @team1.pluck("Season2dateMean")
- 			@team1name = @team1.uniq.pluck("team1")
- 			@team1current = @team1.pluck("CurrentMeanTrend").last
-
+ 			#return team1 + team2 search + names
+ 			@team1 = Stat.search(params[:firstTeam]) 	
  			@team2 = Stat.search(params[:secondTeam])
-  			@team2name = @team2.uniq.pluck("team1")
- 			@team2gt = @team2.pluck("win", "GameMean")
- 			@team2off = @team2.pluck("win", "OFFGameMean")
- 			@team2offVal = @team2.pluck("OFFGameMean")
- 			@team2def = @team2.pluck("win", "DEFGameMean")
- 			@team2defVal = @team2.pluck("DEFGameMean")
- 			@team2s2dm = @team2.pluck("Season2dateMean")
- 			@team2current = @team2.pluck("CurrentMeanTrend").last
+ 			@team1name = @team1.uniq.pluck("team1")
+ 			@team2name = @team2.uniq.pluck("team1") 			
+ 			 			
+ 			#objects to be called by others
+ 			@all = Stat.all
+ 			@team1wk = @team1.order("week").uniq.pluck("week")
+ 			@week = Stat.pluck("week").last
+ 			@year = Stat.pluck("year").first
+
+			#return most recent table values for team1 + team2 in array
+ 			@statArrayTeam1 = @team1.where("year = ?", @year).pluck("year", "week", "win", "team2", "team1pts", "team2pts")
+ 			@team1CurrentStat = @statArrayTeam1.to_a
+ 			@statArrayTeam2 = @team2.where("year = ?", @year).pluck("year", "week", "win", "team2", "team1pts", "team2pts")
+ 			@team2CurrentStat = @statArrayTeam2.to_a
  			
- 			@teamValue1 = @team1.pluck("Season2dateMean", "Season2dateOFFMean", "Season2dateDEFMean").last
- 			@teamValue2 = @team2.pluck("Season2dateMean", "Season2dateOFFMean", "Season2dateDEFMean").last
+			#return team1 + team2 data arrays
+ 			@team1gt = @team1.where("year = ?", @year).pluck("win", "GameMean")
+ 			@team2gt = @team2.where("year = ?", @year).pluck("win", "GameMean")			
+ 			@team1off = @team1.where("year = ?", @year).pluck("win", "OFFGameMean")
+ 			@team2off = @team2.where("year = ?", @year).pluck("win", "OFFGameMean")
+ 			@team1def = @team1.where("year = ?", @year).pluck("win", "DEFGameMean")
+ 			@team2def = @team2.where("year = ?", @year).pluck("win", "DEFGameMean")
+ 			@team1offVal = @team1.where("year = ?", @year).pluck("OFFGameMean")
+ 			@team2offVal = @team2.where("year = ?", @year).pluck("OFFGameMean")
+ 			@team1defVal = @team1.where("year = ?", @year).pluck("DEFGameMean")
+ 			@team2defVal = @team2.where("year = ?", @year).pluck("DEFGameMean")	
+ 			@team1s2dm = @team1.where("year = ?", @year).pluck("Season2dateMean")
+ 			@team2s2dm = @team2.where("year = ?", @year).pluck("Season2dateMean")
  			
- 			@a = Stat.pluck("week").last
- 			@b = Stat.all
- 			@c = @b.group("id", "team1").order("team1")
- 			@d = @c.where("week = ?", @a)
- 			@teamValues = @d.pluck("Season2dateOFFMean", "Season2dateMean", "Season2dateDEFMean")
+ 			#return last value for team1 + team2	
+ 			@teamValue1 = @team1.where("year = ?", @year).pluck("Season2dateMean", "Season2dateOFFMean", "Season2dateDEFMean").last
+ 			@teamValue2 = @team2.where("year = ?", @year).pluck("Season2dateMean", "Season2dateOFFMean", "Season2dateDEFMean").last
+			@team1current = @team1.where("year = ?", @year).pluck("CurrentMeanTrend").last
+ 			@team2current = @team2.where("year = ?", @year).pluck("CurrentMeanTrend").last
+ 
+ 			#return current season2date values by team
+ 				@teamOrder = @all.group("id", "team1").order("team1")
+ 				@currentData = @teamOrder.where("week = ?", @week).where("year = ?", @year)
+ 			@teamValues = @currentData.pluck("Season2dateOFFMean", "Season2dateMean", "Season2dateDEFMean")
 
  		else
  			@stats = Stat.all
