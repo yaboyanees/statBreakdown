94c6e877b37aa9e2e21c221a5bdbcd3db07deebc
yaboyanees <a.merzi@yahoo.com>
2015-09-04 19:46:43 -0400

    cleaned up portfolio page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -55,10 +55,8 @@ class HomeController < ApplicationController
 		 	@team1 = Stat.search(params[:id]) 
 		 	@team2 = @team1.pluck("team2")
  			@teamNames = @team1.uniq.pluck("team1") 
-			
-			#return array of all teams data
-			@rollup = @team1.pluck("year", "week", "GameMean", "Season2dateMean", "CurrentMeanTrend", "OFFGameMean", "Season2dateOFFMean", "CurrentOFFMeanTrend", "DEFGameMean", "Season2dateDEFMean", "CurrentDEFMeanTrend", "win", "team1pts", "team2pts", "diff", "team1", "team2", "short1", "short2", "url")
-			
+ 			@team1wk = @team1.order("week").uniq.pluck("week")
+						
 			#team pt averages by year
 			@teamPts2011 = @team1.where("year = ?", 2011).average("team1pts").round
 			@opponentPts2011 = @team1.where("year = ?", 2011).average("team2pts").round
@@ -68,6 +66,36 @@ class HomeController < ApplicationController
 			@opponentPts2013 = @team1.where("year = ?", 2013).average("team2pts").round
 			@teamPts2014 = @team1.where("year = ?", 2014).average("team1pts").round
 			@opponentPts2014 = @team1.where("year = ?", 2014).average("team2pts").round
+			
+			#league Off average
+			@stat = Stat.all
+			@leagueOPts2011 = @stat.where("win = ?", 'W').where("year = ?", 2011).average("team1pts").round
+			@leagueOPts2012 = @stat.where("win = ?", 'W').where("year = ?", 2012).average("team1pts").round
+			@leagueOPts2013 = @stat.where("win = ?", 'W').where("year = ?", 2013).average("team1pts").round
+			@leagueOPts2014 = @stat.where("win = ?", 'W').where("year = ?", 2014).average("team1pts").round
+		
+			#record by year
+			@win11 = @team1.where("win = ?", 'W').where("year = ?", 2011).count
+			@loss11 = @team1.where("win = ?", 'L').where("year = ?", 2011).count
+
+			@win12 = @team1.where("win = ?", 'W').where("year = ?", 2012).count
+			@loss12 = @team1.where("win = ?", 'L').where("year = ?", 2012).count
+
+			@win13 = @team1.where("win = ?", 'W').where("year = ?", 2013).count
+			@loss13 = @team1.where("win = ?", 'L').where("year = ?", 2013).count
+
+			@win14 = @team1.where("win = ?", 'W').where("year = ?", 2014).count
+			@loss14 = @team1.where("win = ?", 'L').where("year = ?", 2014).count
+			
+			#score differences by win and loss
+			@winValue14 = @team1.where("win = ?", 'W').where("year = ?", 2014).pluck("week", "diff")
+			@lossValue14 = @team1.where("win = ?", 'L').where("year = ?", 2014).pluck("week", "diff")
+			@winValue13 = @team1.where("win = ?", 'W').where("year = ?", 2013).pluck("week", "diff")
+			@lossValue13 = @team1.where("win = ?", 'L').where("year = ?", 2013).pluck("week", "diff")
+			@winValue12 = @team1.where("win = ?", 'W').where("year = ?", 2012).pluck("week", "diff")
+			@lossValue12 = @team1.where("win = ?", 'L').where("year = ?", 2012).pluck("week", "diff")
+			@winValue11 = @team1.where("win = ?", 'W').where("year = ?", 2011).pluck("week", "diff")
+			@lossValue11 = @team1.where("win = ?", 'L').where("year = ?", 2011).pluck("week", "diff")
 		
 		end
 	end
