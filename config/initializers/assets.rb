131f2cf743e847197e90e4657496d5969bff01d3
yaboyanees <a.merzi@yahoo.com>
2015-09-07 19:19:56 -0400

    integrated styles from main website to limit brand loss

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,3 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( highcharts.js )
 Rails.application.config.assets.precompile += %w( highcharts-3d.js )
-Rails.application.config.assets.precompile += %w( lightbox.js )
